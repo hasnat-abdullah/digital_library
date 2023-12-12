bbc0e146b376f9759ab939034b20f5cf23d357cc
hasnat <abdullah.2010bd@gmail.com>
2023-12-12 16:20:34 +0600

    login url name change

--- apps/authentication/api/v1/urls.py
+++ apps/authentication/api/v1/urls.py
@@ -8,8 +8,8 @@ from .views import SignupAPIView, LogoutAPIView
 app_name = 'authentication'
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('signup/', SignupAPIView.as_view(), name='signup'),
     path('logout/', LogoutAPIView.as_view(), name='logout'),
 ]
